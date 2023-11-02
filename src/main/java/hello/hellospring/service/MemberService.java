5d0211e5abd391d7365ff64476846f809b8f1945
koreaygj <ygj1828@gmail.com>
2023-11-02 17:04:59 +0900

    add:test MemberService

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,4 +1,4 @@
-package hello.hellospring.serviece;
+package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
@@ -8,7 +8,11 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     //회원가입
     public Long join(Member member){
@@ -28,5 +32,9 @@ public class MemberService {
                 .ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
     }
 }
